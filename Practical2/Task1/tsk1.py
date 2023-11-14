0c6d8eac0ca1fbcd1c6b7f998d5ed2ec6c65fc46
Zefir3 <chechulin.m@list.ru>
2023-11-14 17:18:05 +0500

    Practice 2 Task 1

---
+++ Practical2/Task1/tsk1.py
@@ -0,0 +1,48 @@
+import numpy as np
+import json
+
+matrix = np.load('matrix_77.npy')
+
+summ = np.sum(matrix)
+avr = np.mean(matrix)
+maxx = matrix.max()
+minn = matrix.min()
+
+sumMd = 0
+avrMd = 0
+mainDiag = np.diag(matrix)
+
+sumSd = 0
+avrSd = 0
+secondDiag = np.diag(np.fliplr(matrix))
+
+frobeniusNorm = np.linalg.norm(matrix, 'fro')
+normalizedMatrix = matrix / frobeniusNorm
+
+for i in mainDiag:
+    sumMd += i
+    avrMd += 1
+avrMd = sumMd / avrMd
+
+for i in secondDiag:
+    sumSd += i
+    avrSd += 1   
+avrSd = sumSd / avrSd
+
+
+
+resultJs = {
+    'sum': int(summ),
+    'avr': int(avr),
+    'sumMD': int(sumMd),
+    'avrMD': int(avrMd),
+    'sumSD': int(sumSd),
+    'avrSD': int(avrSd),
+    'max': int(maxx),
+    'min': int(minn) 
+}
+
+with open('result.json', 'w') as jresult:
+    json.dump(resultJs, jresult)
+
+np.save('normalized_matrix_77', normalizedMatrix)
